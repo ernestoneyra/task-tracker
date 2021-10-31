54f423bb4d0b9ec9563bc018faa8ce65e192a70b
Ernesto Neyra <ernestoneyra31@gmail.com>
2021-10-31 22:47:35 +0100

    Add task and fontawesoem

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
   @Input() text!: string;
-  @Input() color! : string;
+  @Input() color!: string;
+  @Output() btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick() {
+    this.btnClick.emit();
   }
-
 }
