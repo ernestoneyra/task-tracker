2b44aec700144e48d69005690840937f00308a63
Ernesto Neyra <ernestoneyra31@gmail.com>
2021-11-02 14:41:02 +0100

    add toggle and ui service

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,39 +1,46 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {Task} from '../../Task'
+import { Task } from '../../Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
-  @Output() onAddTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
 
-  text!: string
-  day!: string
-  reminder: boolean = false
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
   }
 
+  ngOnInit(): void {}
+
   onSubmit() {
-    if(!this.text){
-      alert('please add a task')
-      return
+    if (!this.text) {
+      alert('please add a task');
+      return;
     }
 
     const newTask = {
       text: this.text,
       day: this.day,
-      reminder: this.reminder
-    }
+      reminder: this.reminder,
+    };
 
-    this.onAddTask.emit(newTask)
+    this.onAddTask.emit(newTask);
 
-    this.text = ''
-    this.day = ''
-    this.reminder = false
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
   }
 }
